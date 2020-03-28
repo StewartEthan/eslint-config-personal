4ec538613a0c9c93378285d2e3a54e8d14b04685
Ethan Stewart <ethanstewart@familysearch.org>
2020-03-28 01:19:50 -0600

    Setup dependencies and tweak rules

--- index.js
+++ index.js
@@ -1,62 +1,59 @@
 module.exports = {
-  plugins: [
-    'babel',
-    'jsx-a11y',
-    'react-hooks',
-  ],
+  plugins: [`babel`, `jsx-a11y`, `react-hooks`],
   extends: [
-    'airbnb',
-    'plugin:jest/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended',
-    'prettier/react',
+    `airbnb`,
+    `plugin:jest/recommended`,
+    `plugin:jsx-a11y/recommended`,
+    `plugin:prettier/recommended`,
+    `prettier/react`,
   ],
-  parser: 'babel-eslint',
+  parser: `babel-eslint`,
   env: {
     browser: true,
     es6: true,
+    node: true,
   },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: `module`,
   },
   // Personal preference overrides
   rules: {
     // Regular ESLint
-    'no-console': ['warn', { allow: ['error'] }],
-    'no-param-reassign': 'off',
-    'no-plusplus': 'off',
-    'no-unused-expressions': 'off',
-    'no-use-before-define': ['error', { functions: false, classes: true, variables: true }],
-    'no-warning-comments': 'off',
-    'prefer-const': ['error', { destructuring: 'all' }],
-    'prefer-destructuring': 'off',
-    'quotes': ['error', 'backtick']
-    
-    // Plugin-specific
-    'babel/no-unused-expressions': ['warn', { allowShortCircuit: true }],
+    'no-console': [`warn`, { allow: [`error`] }],
+    'no-param-reassign': `off`,
+    'no-plusplus': `off`,
+    'no-unused-expressions': `off`,
+    'no-use-before-define': [`error`, { functions: false, classes: true, variables: true }],
+    'no-warning-comments': `off`,
+    'prefer-const': [`error`, { destructuring: `all` }],
+    'prefer-destructuring': `off`,
+    quotes: [`error`, `backtick`],
 
-    'jsx-a11y/label-has-associated-control': ['error', { assert: 'either' }],
+    // Plugin-specific
+    'babel/no-unused-expressions': [`warn`, { allowShortCircuit: true }],
+
+    'jsx-a11y/label-has-associated-control': [`error`, { assert: `either` }],
 
     'prettier/prettier': [
-      'warn',
+      `warn`,
       {
-        arrowParens: 'avoid',
+        arrowParens: `avoid`,
         printWidth: 120,
         semi: false,
         singleQuote: true,
-        trailingComma: 'es5',
+        trailingComma: `es5`,
       },
     ],
 
-    'react/prop-types': 'warn',
-    'react/jsx-filename-extension': 'off',
-    'react/require-default-props': 'off',
+    'react/prop-types': `warn`,
+    'react/jsx-filename-extension': `off`,
+    'react/require-default-props': `off`,
 
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': `error`,
+    'react-hooks/exhaustive-deps': `warn`,
   },
 }
